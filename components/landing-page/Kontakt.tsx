049c0db35a052591f6d3291d255b592e3c8071b1
Omkar Singh <osingh@medienwerft.de>
2026-04-19 13:57:56 +0200

    added google maps iframe

--- components/landing-page/Kontakt.tsx
+++ components/landing-page/Kontakt.tsx
@@ -166,7 +166,7 @@ export default function Kontakt() {
             <div className="relative rounded-xl overflow-hidden border border-border h-64 group">
               <iframe
                 title={t("map.location")}
-                src="https://www.google.com/maps?q=Reeseberg+3,+21079+Hamburg,+Germany&output=embed"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2376.0246857143693!2d9.985808176992455!3d53.450144966951676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b191a08662a451%3A0xd7d3b1b5397f8486!2sTori%20Bau%20GmbH%20Ger%C3%BCstplanen!5e0!3m2!1sde!2sde!4v1732471376758!5m2!1sde!2sde"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
